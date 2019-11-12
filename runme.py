1c51f065cedbcacc6006f87a1f0859e178689df0
Donna Dietz <donna.dietz@gmail.com>
2019-11-11 19:20:35 -0500

    WHops!

--- runme.py
+++ runme.py
@@ -1,12 +1,12 @@
 # Fall 2019
-# HW 6
+# HW 7
 # Keep this file in the folder with the .pyc files
 # Run this file, or type "from runme import *"
-# You should then be able to run the game by typing
-# key.playGame()
+# You should then be able to run the code
+# key.main()
 # However, if you know your python version, for example 3.7,
 # you can also play this game at the command line by typing
-# python3 hw6_example37.pyc
+# python3 (filename).pyc
 # etc
 
 import os
@@ -15,7 +15,7 @@ import re
 def findFileNames():
     L=[]
     cp=os.listdir(".")
-    p=re.compile('hw6[\w]*3[\d]')
+    p=re.compile('hw[\w]*3[\d]')
     for i in range(len(cp)):
         names=p.findall(cp[i])
         if not(names in L) and len(names)>0:
